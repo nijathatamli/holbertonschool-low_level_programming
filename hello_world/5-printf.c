49047c4d994878e40a0c80b161d288a231437860
Nijat Hatamli <hatamlinijat@gmail.com>
2025-07-23 16:58:12 +0000

    5th Updated

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-/*
-* main entry point
-* Return always will be 0
-*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
