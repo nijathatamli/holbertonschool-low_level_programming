e3e8145c2395d010512e3edd830cd353ad2dfbb0
Nijat Hatamli <hatamlinijat@gmail.com>
2025-07-23 17:20:22 +0000

    6th updated

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,7 +6,7 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 int a;
 long int b;
@@ -16,7 +16,7 @@ float e;
 
 printf("Size of a char: %lu byte(s)\n", sizeof(d));
 printf("Size of an int: %lu byte(s)\n", sizeof(a));
-printf("Size of a long int: %lu byte(s)\n",sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", sizeof(b));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
 printf("Size of a float: %lu byte(s)\n", sizeof(e));
 
