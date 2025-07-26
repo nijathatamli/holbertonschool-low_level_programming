ff86a1ac599ab86c29c9f22393d94dc67fee4a5b
Nijat <hatamlinijat@gmail.com>
2025-07-26 20:00:06 +0000

    exists last number about

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,33 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Prints the last digit of a random number
+ *        and whether it is greater than 5, 0, or less than 6 and not 0.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if ((n%10)>5)
+	last_digit = n % 10;
+
+	if (last_digit > 5)
 	{
-		printf("Last digit of %d is %d and is greaater than 5\n",n,n%10);
-	
+		printf(Last digit of %d is %d and is greater than 5n, n, last_digit);
 	}
-	
-	else if ((n%10)<6 && (n%10)!=0)
+	else if (last_digit == 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",n,n%10);
-	
+		printf(Last digit of %d is %d and is 0n, n, last_digit);
 	}
-	
 	else
 	{
-		printf("Last digit of %d is %d and is 0\n",n,n%10);
-	
+		printf(Last digit of %d is %d and is less than 6 and not 0n, n, last_digit);
 	}
-	
+
 	return (0);
 }
+
