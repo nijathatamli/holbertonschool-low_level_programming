427bdbe4912e2ba1e746221d166c7259ce46cb2e
Nijat Hatamli <hatamlinijat@gmail.com>
2025-07-27 09:27:44 +0000

    exists aplhaped exists q and e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,25 @@
 /*
- * File: 3-print_alphabets.c
- * Auth: Nijat Hatamli
+ * File: 4-print_alphabt.c
+ * Auth: Brennan D Baraban
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase, and then in uppercase.
+ * main - Prints the alphabet in lowercase, except for q and e.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-        char letter;
+	char letter;
 
-	for (letter= 'a' ; letter<= 'z' ; letter++)
-	  {
-	    if (letter!= 'e' && letter!= 'q' )
-	      putchar(letter);
-	  }  
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
 	putchar('\n');
 
 	return (0);
