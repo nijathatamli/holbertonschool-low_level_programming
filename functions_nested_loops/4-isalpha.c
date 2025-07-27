48fa1e21e9f10a5480e35990049d0aee46ebb2fa
Nijat <hatamlinijat@gmail.com>
2025-07-27 19:29:04 +0000

    nothing

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,13 +1,22 @@
-#include "holberton.h"
+#include "../holberton.h"
+
 /**
- * _isalpha - a function that checks for alphabetic character
- * @c: single letter input
- * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-int _isalpha(int c)
+int main(void)
 {
-	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
-		return (1);
-	else
-		return (0);
+	int r;
+
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
 }
