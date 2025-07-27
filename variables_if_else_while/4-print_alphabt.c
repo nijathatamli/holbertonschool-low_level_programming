7e24393e09a6ca5e3e502c29f923a8ee809c899e
Nijat Hatamli <hatamlinijat@gmail.com>
2025-07-27 08:39:45 +0000

    expected q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+/*
+ * File: 3-print_alphabets.c
+ * Auth: Nijat Hatamli
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        char letter;
+
+	for (letter='a'; letter<='z'; letter++)
+	  {
+	    if (letter!='e' && letter!='q')
+	      putchar(letter);
+	  }  
+	putchar('\n');
+
+	return (0);
+}
