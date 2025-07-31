3ec7779a39e50d891ca0e1616a2fb89898407a46
Nijat <hatamlinijat@gmail.com>
2025-07-31 11:08:58 +0000

    5th commited

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,16 @@ void more_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <15; i++)
+	for (i = 0; i <10; i++)
 	{
-		_putchar(i);
+		for (j = 0; i < 15; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar(j/10 + "0");	
+			}
+			_putchar(j % 10 + '0')
+		}
 	}
 	_putchar('\n');
 
