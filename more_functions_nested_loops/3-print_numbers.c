dd32cea5edb5eb37364f03c7b9640a2926140ae4
Nijat <hatamlinijat@gmail.com>
2025-07-31 08:21:23 +0000

    3th print numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,16 @@
 #include "main.h"
-/** 
- * print_numbers -Entry point
+/**
+ * print_numbers - Entry point
  *
- * Return: Always 0 (Succes)
+ * Return: Always 0 (Success)
  */
 void print_numbers(void)
 {
 	int i;
-	
 
- 	for (i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
-	
