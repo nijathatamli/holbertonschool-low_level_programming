f4e1ff2f2a8690520d4e1e1219e2992ede0f3712
Nijat <hatamlinijat@gmail.com>
2025-07-31 10:38:22 +0000

    5th commited

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * more_numbers - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <15; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
