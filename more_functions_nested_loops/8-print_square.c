3141b5b1098753c48f1e4863878216235ec3dbed
Nijat <hatamlinijat@gmail.com>
2025-08-01 13:20:28 +0000

    print line 2

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - Entry point
+ * @size: var
+ * Return: Always 0 (Success)
+ */
+void print_square(int s)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < s; i++)
+		{
+			for (j = 0; j < s; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n')
+		}
+	}
+}
