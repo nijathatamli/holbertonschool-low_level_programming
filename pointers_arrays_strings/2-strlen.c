fb1cd9f38a71984ce8386b1eddfafbed53149795
Nijat <hatamlinijat@gmail.com>
2025-08-03 06:35:44 +0000

    change value by pointer

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - Entry point
+ * @x: description for x
+ * @y: description for y
+ *
+ * Return: Always 0 (Success)
+ */
+int swap_int(int *x, int *y)
+{
+		int a;
+		a = *x;
+		*x = *y;
+		*y = d;
+}
