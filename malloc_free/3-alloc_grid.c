d16f70df308b37db0e98a2006e4465d26cac97a2
Nijat <hatamlinijat@gmail.com>
2025-08-17 10:32:06 +0000

    4th question is commted

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - function for print 2d array
+ *
+ * @width: - width
+ * @height: - height
+ *
+ * Return: Always 0
+ */
+int **alloc_grid(int width, int height)
+{
+	int **array i = 0, j = 0;
+	if (width <= 0 || height <= 0)
+		return (NULL)
+	array = malloc(height * sizeof(int *));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(width * sizeof(int));
+		if (array[i == NULL])
+			for (j = 0; j < i; j++)
+				free(array[j]);
+		free(array);
+		return (NULL);
+	}
+	return (array);
+}
