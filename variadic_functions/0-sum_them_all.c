eff3e59f7bde2faa622431657866d3546c977bba
Nijat <hatamlinijat@gmail.com>
2025-08-24 14:13:47 -0500

    sum all elemnts

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <strdarg.h>
+#include "varidic_function.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: - num
+ * Return: Always 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+int s = 0;
+
+va_start(args, n);
+if (n == 0)
+return (0);
+for (i = 0; i < n; i++)
+s += va_arg(args,int);
+va_end(args);
+return (s);
+}
---
+++ variadic_functions/README.md
@@ -0,0 +1,1 @@
+Hey there guy, if you wanna get huge explanation, you need come back main page
