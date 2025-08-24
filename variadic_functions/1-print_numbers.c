ca66a0ea2ccfdb6bd129b1a6dd6b791e23dda736
Nijat <hatamlinijat@gmail.com>
2025-08-24 14:46:28 -0500

    finshed all of them

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ unsigned int i;
 va_start(args, n);
 for (i = 0; i < n; i++)
 {
-printf("%d", va_arg(args,int));
+printf("%d", va_arg(args, int));
 if (separator != NULL && i != n - 1)
 printf("%s", separator);
 }
