1829276b5f31607d4cc602a32489f000be097238
root <root@localhost>
2025-09-01 10:39:07 +0400

    check latter of lower or upper

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * _isalpha - Entry point
+ * @c: variable
+ *
+ * Return: Always 0 or 1 (Success)
+ */
+int _islower(int c)
+{
+    if (c >= 97 && c <= 122)
+    {
+        return (1);
+    }
+    return (0);
+}
