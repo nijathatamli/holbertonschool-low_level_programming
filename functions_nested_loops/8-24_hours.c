c86815e9fe42854b3d636d791ddd1da6cf9f6ffe
root <root@localhost>
2025-09-02 14:25:57 +0400

    edited again 8th task

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,41 +1,36 @@
 #include "main.h"
-/**
- * jack_bauer - Entry point
- *
- * Return: Always 0 (Success)
- */
 void jack_bauer(void)
 {
-int  a = 0, b = 0;
+    int  a = 0, b = 0;
     while (a <= 24)
     {
         while (b <= 60)
         {
             if (a <=9)
             {
-                putchar('0');
-                putchar(a % 10 + '0');
+                _putchar('0');
+                _putchar(a % 10 + '0');
             }
             else
             {
-                putchar(a / 10 + '0');
-                putchar(a % 10 + '0');
+                _putchar(a / 10 + '0');
+                _putchar(a % 10 + '0');
             }
-            putchar(':');
+            _putchar(':');
             if (b <= 9)
             {
-                putchar('0');
-                putchar(b % 10 + '0');
+                _putchar('0');
+                _putchar(b % 10 + '0');
             }
             else
             {
-                putchar(b / 10 + '0');
-                putchar(b % 10 +'0');
+                _putchar(b / 10 + '0');
+                _putchar(b % 10 +'0');
             }
+            _putchar('\n');
             b++;
-            putchar('\n');
         }
-        a++;
         b = 0;
+        a++;
     }
 }
