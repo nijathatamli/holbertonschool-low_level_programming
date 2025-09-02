1cc93866a4bb4f2a0c8125339028bf886f11c0c5
root <root@localhost>
2025-09-02 14:51:37 +0400

    nothing at this tome

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,36 +1,36 @@
 #include "main.h"
 void jack_bauer(void)
 {
-    int  a = 0, b = 0;
-    while (a <= 23)
-    {
-        while (b <= 59)
-        {
-            if (a <=9)
-            {
-                _putchar('0');
-                _putchar(a % 10 + '0');
-            }
-            else
-            {
-                _putchar(a / 10 + '0');
-                _putchar(a % 10 + '0');
-            }
-            _putchar(':');
-            if (b <= 9)
-            {
-                _putchar('0');
-                _putchar(b % 10 + '0');
-            }
-            else
-            {
-                _putchar(b / 10 + '0');
-                _putchar(b % 10 +'0');
-            }
-            _putchar('\n');
-            b++;
-        }
-        b = 0;
-        a++;
-    }
+int  a = 0, b = 0;
+while (a <= 23)
+{
+while (b <= 59)
+{
+if (a <=9)
+{
+_putchar('0');
+_putchar(a % 10 + '0');
+}
+else
+{
+_putchar(a / 10 + '0');
+_putchar(a % 10 + '0');
+}
+_putchar(':');
+if (b <= 9)
+{
+_putchar('0');
+_putchar(b % 10 + '0');
+}
+else
+{
+_putchar(b / 10 + '0');
+_putchar(b % 10 +'0');
+}
+_putchar('\n');
+b++;
+}
+b = 0;
+a++;
+}
 }
