71797935174bcbccc8803f23ec04a8c6f86afde4
root <root@localhost>
2025-09-03 15:18:21 +0400

    edited for betty

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,5 +1,12 @@
 #include "main.h"
 int add(int num1, int num2)
+/**
+ * add - Entry point
+ * @num1: variable
+ * @num2: variable2
+ *
+ * Return: Always 0 (Success)
+ */
 {
-return (num1 + num2); 
+return (num1 + num2);
 }
