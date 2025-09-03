9cfcce66699147984da027d5e2ab719aaab67566
root <root@localhost>
2025-09-03 19:11:03 +0400

    changed an again for betty

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,22 +1,31 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98,
+ *               followed by a new line.
+ * @n: Starting integer
+ *
+ * Description: Numbers are separated by a comma and space.
+ *              The function handles both increasing and decreasing order.
+ */
 void print_to_98(int n)
 {
-    if (n > 98)
-    {
-        while (n > 98)
-    {
-        printf("%d, ", n);
-        n--;
-    }
-    }
-    else if (n < 98)
-    {
-        while (n < 98)
-        {
-            printf("%d, ", n);
-            n++;
-        }
-    }
-    printf("98\n");
+	if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	else if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	printf("98\n");
 }
