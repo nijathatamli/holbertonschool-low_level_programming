7b2daac7f27f85651441b55caaff002a5c42f0ae
root <root@localhost>
2025-09-03 17:27:55 +0400

    complate 11th task without bety

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+void print_to_98(int n)
+{
+if (n < 98)
+{
+int s = 0;
+while (s <= 98)
+{
+if (s < 10)
+{
+_putchar(s + '0');
+}
+else
+{
+_putchar((s / 10) + '0');
+_putchar((s % 10) + '0');
+}
+if (s != 98)
+{
+_putchar(', ');
+}
+s++;
+}
+}
+if (n >= 98)
+{
+while (n > 0)
+{
+_putchar(n % 10 + '0');
+n = n / 10;
+}
+n--;
+}
+_putchar('\n');
+}
