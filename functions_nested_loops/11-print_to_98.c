67fe698bad9ba969462fe20885249311f0d512f5
root <root@localhost>
2025-09-03 19:09:09 +0400

    edited an again 11th task

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@ void print_to_98(int n)
 {
     if (n > 98)
     {
-        while (n >= 98)
+        while (n > 98)
     {
         printf("%d, ", n);
         n--;
@@ -18,8 +18,5 @@ void print_to_98(int n)
             n++;
         }
     }
-    else
-    {
-        printf("98");
-    }
+    printf("98\n");
 }
