c6a6fb362b32a58c65c72dc06ace5e9083e14676
root <root@localhost>
2025-09-03 19:01:38 +0400

    writed 11th task witout complated

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,41 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 void print_to_98(int n)
 {
-if (n < 98)
-{
-int s = 0;
-while (s <= 98)
-{
-if (s < 10)
-{
-_putchar(s + '0');
-}
-else
-{
-_putchar((s / 10) + '0');
-_putchar((s % 10) + '0');
-}
-if (s != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
-s++;
-}
-}
-if (n > 98)
-{
-while (n > 0)
-{
-_putchar(n % 10 + '0');
-n = n / 10;
-}
-n--;
-}
-else
-{
-_putchar('9');
-_putchar('8');
-}
-_putchar('\n');
+    if (n > 98)
+    {
+        while (n >= 98)
+    {
+        printf("%d, ", n);
+        n--;
+    }
+    }
+    else if (n < 98)
+    {
+        while (n < 98)
+        {
+            printf("%d, ", n);
+            n++;
+        }
+    }
 }
