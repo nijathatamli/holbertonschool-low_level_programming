dd2d2c20c759ce3ea69b939c8e1be62c6b01c7ab
root <root@localhost>
2025-09-05 15:02:57 +0400

    complated 2th task by bety documentation and style

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,13 +1,24 @@
 #include "main.h"
 
+/**
+ * _strchr - locates the first occurrence of a character in a string
+ * @s: the string to search in
+ * @c: the character to locate
+ *
+ * Return: pointer to the first occurrence of c in s,
+ * or NULL if c is not found
+ */
 char *_strchr(char *s, char c)
 {
-    int i = 0;
-    while (s[i] != c && s[i] != c)
-    {
-        s[i] = '\0';
-    }
-    if (s[i] == '\0')
-        return (NULL);
-    return (s + i);
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	if (*s == '\0')
+	{
+		return (NULL);
+	}
 }
