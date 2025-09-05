4c2afe0416e2d2b74ec2a7bd9fe2316d79fb3892
root <root@localhost>
2025-09-05 15:09:44 +0400

    insallah final edit for it

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
-		s++;
-	}
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
+s++;
+}
+if (c == '\0')
+return s;
 
-	
-		return (NULL);
+return (NULL);
 }
