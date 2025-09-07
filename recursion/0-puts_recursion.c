01153b4ba7d745d177dcaa14bcb754a4c801edea
Nijat <129670358+nijathatamli@users.noreply.github.com>
2025-09-07 17:10:07 +0400

    Update 0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
  * _puts_recursion - check the code
- * @s: - s is entry value, we are gonna read words in the s value
+ * @s: - s
  * Return: Always 0.
  */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')  
+if (*s == '\0')
+{
+_putchar('\n');
 return;
-_putchar(*s);             
-_puts_recursion(s + 1);   
+}
+_putchar(*s);
+_puts_recursion(++s);
 }
