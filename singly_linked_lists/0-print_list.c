ecc5c84e8e83892a6a3dc884ced617476a29af36
root <root@localhost>
2025-09-08 20:23:15 +0400

    first task is complated in Singly Linked list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - a function that prints all the elements of a list_t list.
+ * @h: header node of singly linked list
+ * Return: A number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+    int count = 0;
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+        {   
+            printf("[0] (nil)\n");
+            h = h->next;
+            count++;
+        }
+        else
+        {
+            printf("[%d] %s\n", h->len, h->str);
+            h = h->next;
+            count++;
+        }
+    }
+    return (count);
+}
