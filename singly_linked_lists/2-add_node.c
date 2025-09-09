812726fecccda01d9d1e405a6aa59e8b10c281c7
root <root@localhost>
2025-09-09 18:25:46 +0400

    third task is complated without betty, it is adding in linked list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * add_node - function that adds a new node at the beginning of a list_t list.
+ * @head: header node of singly linked list
+ * @str: string
+ * Return: A number of nodes
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+    int len;
+    
+    for (len = 0; str[len]; len++)
+    {
+        list_t *new_node;
+        new_node = malloc(sizeof(list_t));
+        if (str[len] != NULL)
+            return (NULL);
+        new_node->str = strdup(str);
+        new_node->len = len;
+        new_node->next = *head;
+        *head = new_node;
+        return (*head); 
+
+        
+    }
+}
