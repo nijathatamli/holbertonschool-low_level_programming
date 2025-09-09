e9d0a6783b8a583c0c54dbb3f800d5cc66e1b845
root <root@localhost>
2025-09-09 20:21:28 +0400

    edited an again

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,44 +1,44 @@
 #include "lists.h"
 #include <string.h>
-#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * add_node_end - a function that adds a new node at the end of a list_t list.
- * @head: header node of singly linked list
- * @str: string
- * Return: address of the new element
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to head of singly linked list
+ * @str: string to duplicate into the new node
+ * Return: address of the new element, or NULL on failure
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
+    list_t *new_node, *temp;
     int lens = 0;
-    list_t *new_node;
-    list_t *temp;
 
-    for (lens = 0; str[lens]; lens++)
+    while (str[lens])
+        lens++;
+
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
     {
-        new_node = malloc(sizeof(list_t));
-        if (new_node == NULL)
-        {
-            return (NULL);
-        }
-        new_node->str = strdup(str);
-        if (new_node->str == NULL)
-        {
-            free(new_node);
-            return (NULL);
-        }
-        new_node->len = lens;
-        if (*head == NULL)
-        {
-            *head = new_node;
-        }
-        temp = *head;
-        while (temp->next != NULL)
-        {
-            temp = temp->next;
-        }
-        temp->next = new_node;
-        new_node->next =NULL;
+        free(new_node);
+        return (NULL);
+    }
+    new_node->len = lens;
+    new_node->next = NULL;
+
+    if (*head == NULL)
+    {
+        *head = new_node;
         return (new_node);
     }
+
+    temp = *head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = new_node;
+    return (new_node);
 }
