028d9677e41631865749966b966ca8b8df378aa0
root <root@localhost>
2025-09-09 15:54:27 +0400

    second task is print len of linked list, it is already complated by myself

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -6,21 +6,22 @@
  */
 size_t print_list(const list_t *h)
 {
-    int count = 0;
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-        {   
-            printf("[0] (nil)\n");
-            h = h->next;
-            count++;
-        }
-        else
-        {
-            printf("[%d] %s\n", h->len, h->str);
-            h = h->next;
-            count++;
-        }
-    }
-    return (count);
+int count = 0; 
+while (h != NULL)
+{
+if (h->str == NULL)
+{   
+printf("[0] (nil)\n");
+h = h->next;
+count++;
 }
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+count++;
+}
+}
+return (count);
+}
+s
---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: header node of singly linked list
+ * Return: A number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+int count = 0;
+while (h != NULL)
+{
+count++;
+h->next;
+}
+return(count);
+}
