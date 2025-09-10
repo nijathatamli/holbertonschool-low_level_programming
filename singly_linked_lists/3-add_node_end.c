95b1ba6daab66f915c9705f64e8052c330ff033e
root <root@localhost>
2025-09-10 14:53:09 +0400

    everthing is finshed as well last 2 tasks is complated by betty

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,37 +11,37 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *temp;
-    int lens = 0;
+list_t *new_node, *temp;
+int lens = 0;
 
-    while (str[lens])
-        lens++;
+while (str[lens])
+lens++;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+new_node->str = strdup(str);
+if (new_node->str == NULL)
+{
+free(new_node);
+return (NULL);
+}
 
-    new_node->len = lens;
-    new_node->next = NULL;
+new_node->len = lens;
+new_node->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+if (*head == NULL)
+{
+*head = new_node;
+return (new_node);
+}
 
-    temp = *head;
-    while (temp->next != NULL)
-        temp = temp->next;
+temp = *head;
+while (temp->next != NULL)
+temp = temp->next;
 
-    temp->next = new_node;
+temp->next = new_node;
 
-    return (new_node);
+return (new_node);
 }
