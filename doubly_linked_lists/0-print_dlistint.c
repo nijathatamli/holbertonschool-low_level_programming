00792d307c540ff86067110c4e30e1b2d8ab0447
root <root@localhost>
2025-09-14 16:36:02 +0400

    first task in Doubly linked list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_dlistint - a function that prints all the elements of  list.
+ * @h: pointer named head and this first node
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+const dlistint_t *pdl = h;
+
+int count = 0;
+while (pdl != NULL)
+{
+printf("%d\n", pdl->n);
+pdl = pdl->next;
+count++; 
+}
+return (count);
+}
---
+++ doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Hello there, if you wanna read huge README file, you have gotta main page, you are gonna find readable README file
