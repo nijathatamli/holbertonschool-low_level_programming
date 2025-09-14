5e0b55cd9dde1a748e9975ef20ff3451a9157d21
root <root@localhost>
2025-09-14 18:13:04 +0400

    second task is not complated

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,12 +1,17 @@
 #include "lists.h"
+/**
+ * dlistint_len - a function for print  number of elements in a linked list
+ * @h: pointer named head and this first node
+ * Return: the len of nodes
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
-    const dlistint_t *ld = h;
-    int len = 0;
-    while (ld != NULL)
-    {
-        len++;
-        ld = ld->next; 
-    }
-    return (len);
+const dlistint_t *ld = h;
+int len = 0;
+while (ld != NULL)
+{
+len++;
+ld = ld->next;
+}
+return (len);
 }
