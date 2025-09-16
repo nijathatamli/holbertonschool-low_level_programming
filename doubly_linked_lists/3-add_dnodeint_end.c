e4efd8d7b57c129e3b071f17afb651df89acd9b5
root <root@localhost>
2025-09-16 18:56:32 +0400

    task is add element end of doubly linked list

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - adds a new node at the end of a doubly linked list
+ * @head: pointer to the head of the list
+ * @n: value to insert
+ *
+ * Return: address of the new element, or NULL on failure
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+    dlistint_t *new_node, *temp;
+    temp = *head;
+    new_node = malloc(sizeof(new_node));
+    if (new_node == NULL)
+        return ("Segmentation fault");
+    new_node->n = n;
+    new_node->next = NULL;
+    if (*head == NULL)
+    {
+        new_node->prev = NULL;
+        *head = new_node;
+        return (new_node);
+    }
+    while (temp->next != NULL)
+    {
+         temp = temp->next;
+    }
+    temp->next = new_node;
+    new_node->prev = temp;
+    return (new_node);
+}
