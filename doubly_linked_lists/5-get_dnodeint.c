b6873f22322a1247f1e27bdc0559798a77fb5514
root <root@localhost>
2025-09-19 20:11:07 +0400

    finshed all of tasks

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -10,16 +10,16 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    dlistint_t *temp = head;
+	dlistint_t *temp = head;
 
-    if (head == NULL)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    while (temp != NULL && index > 0)
-    {
-        temp = temp->next;
-        index--;
-    }
+	while (temp != NULL && index > 0)
+	{
+		temp = temp->next;
+		index--;
+	}
 
-    return (temp);
+	return (temp);
 }
