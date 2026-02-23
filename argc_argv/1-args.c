3137f9863d78c8126f807eed1942e4201205c5cb
Nijat <hatamlinijat@gmail.com>
2026-02-24 00:12:41 +0400

    finshed all tasks

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-    (void)argv;   /* unused warning olmasın deyə */
-    printf("%d\n", argc - 1);
-    return 0;
+(void)argv;   /* unused warning olmasın deyə */
+printf("%d\n", argc - 1);
+return 0;
 }
