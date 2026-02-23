a6da2bf96b7a926079d1eb1434a1cb500758efb0
Nijat <hatamlinijat@gmail.com>
2026-02-24 01:14:52 +0400

    string nconcat is ready

---
+++ more_malloc_free/main.h
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void rev_string(char *s);
+void puts_half(char *str);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
